c7bab119b68d507598d85f01098cd854b62d8e51
seruva19 <seruva19@yandex.ru>
2023-04-30 18:58:37 +0300

    update max image dimension to 2048

--- src/shared/params.py
+++ src/shared/params.py
@@ -1,6 +1,6 @@
 image_width_min = 16
 image_height_min = 16
-image_width_max = 1024
-image_height_max = 1024
+image_width_max = 2048
+image_height_max = 2048
 image_width_step = 16
 image_height_step = 16
