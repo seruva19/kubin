fix incorrect image shape calculation while outpainting

src/models/model_mock.py
@@ -50,7 +50,7 @@ class Model_Mock:
   def outpaint(self, params):
     print('mock outpaint executed')
     image = params['image'] 
-    old_w, old_h = image.size
+    image_w, image_h = image.size
     
     offset = params['offset']
 
@@ -58,23 +58,16 @@ class Model_Mock:
       top, right, bottom, left = offset
       inferred_mask_size = tuple(a + b for a, b in zip(image.size, (left + right, top + bottom)))[::-1]
       mask = np.zeros(inferred_mask_size, dtype=np.float32)
-      mask[top:old_h+top, left:old_w+left] = 1
+      mask[top:image_h+top, left:image_w+left] = 1
       image = ImageOps.expand(image, border=(left, top, right, bottom), fill=0)
 
     else:
-      output_size = (params['w'], params['h'])
-      image = image.resize(output_size)
       x1, y1, x2, y2 = image.getbbox()
-      w_factor = params['w'] / old_w
-      h_factor = params['h'] / old_h
-
-      x1 *= w_factor
-      x2 *= w_factor
-      y1 *= h_factor
-      y2 *= h_factor
-
-      mask = np.zeros(image.size, dtype=np.float32)
-      mask[int(y1):int(y2), int(x1):int(x2)] = 1
+      mask = np.ones((image_h, image_w), dtype=np.float32)
+      mask[0:y1,:] = 0
+      mask[:,0:x1] = 0
+      mask[y2:image_h,:] = 0
+      mask[:,x2:image_w] = 0
     
     infer_size = params['infer_size']
     if infer_size:
@@ -83,7 +76,10 @@ class Model_Mock:
       width=params['w']
       height=params['h']
 
-    return [image, Image.fromarray(np.uint8(mask * 255))]
+    mask_img = Image.fromarray(np.uint8(mask * 255)).resize((width, height), resample=Image.LANCZOS)
+    image = image.resize(mask_img.size, resample=Image.LANCZOS)
+
+    return [image, mask_img]
   
   def dummyImages(self):
     screenshots_as_dummies = [entry.name for entry in os.scandir('sshots') if entry.is_file()]