468e1132d9eb88fea2d397ed426ef59809bd190d
seruva19 <seruva19@yandex.ru>
2023-05-17 23:02:58 +0300

    change filename scheme of saved images
    https://github.com/seruva19/kubin/discussions/67

--- src/utils/file_system.py
+++ src/utils/file_system.py
@@ -7,9 +7,18 @@ def save_output(output_dir, task_type, images, seed):
   for img in images:
     path = f'{output_dir}/{task_type}'
     if not os.path.exists(path): os.makedirs(path)
-  
-    name = f'{path}/{seed}-{uuid.uuid4()}.png'
-    img.save(name, 'PNG')
-    output.append(name)
+
+    current_datetime = datetime.now()
+    format_string = "%Y%m%d%H%M%S"
+    formatted_datetime = current_datetime.strftime(format_string)
+
+    # name = f'{path}/{seed}-{uuid.uuid4()}.png'
+    filename = f'{path}/{formatted_datetime}.png'
+    while os.path.exists(filename):
+      unique_id = current_datetime.microsecond
+      filename = f'{formatted_datetime}_{unique_id}.jpg'
+
+    img.save(filename, 'PNG')
+    output.append(filename)
 
   return output
