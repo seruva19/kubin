d0e609c9a44dd932f8aa7b294d47e0c61e1aa27b
seruva19 <seruva19@yandex.ru>
2023-05-26 18:13:53 +0300

    fix formatting

--- extensions/kd-training/setup_ext.py
+++ extensions/kd-training/setup_ext.py
@@ -4,47 +4,50 @@ from train_modules.train_prior_ui import train_prior_ui
 from train_modules.train_unclip_ui import train_unclip_ui
 from train_modules.train_tools import train_tools_ui
 
+
 def setup(kubin):
-  def training_ui(ui_shared, ui_tabs):
-    with gr.Column() as training_block:
-      with gr.Tabs() as training_tabs:
-        with gr.TabItem('Prior', id='training-prior'):
-          prior_ui = train_prior_ui(kubin, training_tabs)
+    def training_ui(ui_shared, ui_tabs):
+        with gr.Column() as training_block:
+            with gr.Tabs() as training_tabs:
+                with gr.TabItem("Prior", id="training-prior"):
+                    prior_ui = train_prior_ui(kubin, training_tabs)
 
-        with gr.TabItem('UnCLIP', id='training-unclip'):
-          unclip_ui = train_unclip_ui(kubin, training_tabs)
-        
-        with gr.TabItem('Tools', id='training-tools'):
-          utils_ui = train_tools_ui(kubin)
+                with gr.TabItem("UnCLIP", id="training-unclip"):
+                    unclip_ui = train_unclip_ui(kubin, training_tabs)
 
-    return training_block
-  
-  def inference_ui(target):
-    with gr.Column() as model_block:
-      with gr.Tab('Checkpoint'):
-        model = gr.Dropdown(choices=[], label='Prior')
-        model = gr.Dropdown(choices=[], label='Unclip')
+                with gr.TabItem("Tools", id="training-tools"):
+                    utils_ui = train_tools_ui(kubin)
 
-    return model_block, model
-  
-  def apply_model(params, model):
-    print(f'applying model {model}') 
-    
-    return params
+        return training_block
 
-  def settings_ui():
-    with gr.Column() as settings_block:
-      None
+    def inference_ui(target):
+        with gr.Column() as model_block:
+            with gr.Tab("Checkpoint"):
+                model = gr.Dropdown(choices=[], label="Prior")
+                model = gr.Dropdown(choices=[], label="Unclip")
 
-    return settings_block
-  
-  return {
-    'title': 'Training',
-    'targets': ['t2i', 'i2i', 'mix', 'inpaint', 'outpaint'],
-    'tab_ui': lambda ui_s, ts: training_ui(ui_s, ts),
-    'inject_title': 'Model',
-    # 'inject_ui': lambda target: inference_ui(target),
-    'inject_fn': lambda target, params, augmentations: apply_model(params, augmentations[0]),
-    'hook_fn': lambda h: None,
-    'settings_ui': lambda target: settings_ui()
-  }
+        return model_block, model
+
+    def apply_model(params, model):
+        print(f"applying model {model}")
+
+        return params
+
+    def settings_ui():
+        with gr.Column() as settings_block:
+            None
+
+        return settings_block
+
+    return {
+        "title": "Training",
+        "targets": ["t2i", "i2i", "mix", "inpaint", "outpaint"],
+        "tab_ui": lambda ui_s, ts: training_ui(ui_s, ts),
+        "inject_title": "Model",
+        # 'inject_ui': lambda target: inference_ui(target),
+        "inject_fn": lambda target, params, augmentations: apply_model(
+            params, augmentations[0]
+        ),
+        "hook_fn": lambda h: None,
+        "settings_ui": lambda target: settings_ui(),
+    }
