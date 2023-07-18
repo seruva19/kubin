551f66bcff984b596fafbfc5af0ec8d4104c687d
seruva19 <seruva19@yandex.ru>
2023-07-18 20:23:51 +0300

    add shebang to .sh scripts

--- install-torch.sh
+++ install-torch.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source venv/bin/activate
 pip uninstall -y torch
 pip install torch --index-url https://download.pytorch.org/whl/cu118 --force-reinstall --no-deps
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt --no-cache-dir --ignore-installed --force-reinstall
