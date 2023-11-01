713ed51165c29d2e5162044748edab6f5238673d
seruva19 <seruva19@yandex.ru>
2023-11-01 13:18:06 +0300

    Create prompt_weights.py

---
+++ src/model_utils/prompt_weights.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+from typing import Optional, Tuple, Union
+from diffusers.schedulers.scheduling_utils import SchedulerOutput
+
+
+def create_weighted_embeds(pipeline, sampler, task="text2img"):
+    pass
