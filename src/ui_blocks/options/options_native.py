5149af1c00b4083751f6e2cd693be67937dcb356
seruva19 <seruva19@yandex.ru>
2024-02-02 22:59:48 +0300

    add option to use custom text encoder for native K3 pipeline, change default encoder
    - see https://github.com/ai-forever/Kandinsky-3/issues/16
    - 'pszemraj/flan-ul2-text-encoder' set as default encoder
    + fix some errors

--- src/ui_blocks/options/options_native.py
+++ src/ui_blocks/options/options_native.py
@@ -6,9 +6,28 @@ def options_tab_native(kubin: Kubin):
     on_change = "(key, value, requiresRestart) => kubin.utils.optionsChanged(key, value, requiresRestart)"
 
     with gr.Column(elem_classes=["options-block", "options-native"]) as native_options:
+        text_encoder = gr.Dropdown(
+            value=lambda: kubin.params("native", "text_encoder"),
+            choices=["google/flan-ul2", "pszemraj/flan-ul2-text-encoder"],
+            label="Text encoder",
+            allow_custom_value=True,
+            elem_classes=["options-medium"],
+        )
+
         native = gr.Checkbox(
             value=lambda: kubin.params("native", "flash_attention"),
             label="Use Flash Attention",
+        )
+
+        text_encoder.change(
+            fn=None,
+            _js=on_change,
+            inputs=[
+                gr.Text("native.text_encoder", visible=False),
+                text_encoder,
+                gr.Checkbox(False, visible=False),
+            ],
+            show_progress=False,
         )
 
         native.change(
@@ -17,7 +36,7 @@ def options_tab_native(kubin: Kubin):
             inputs=[
                 gr.Text("native.flash_attention", visible=False),
                 native,
-                gr.Checkbox(False, visible=False),
+                gr.Checkbox(True, visible=False),
             ],
             show_progress=False,
         )
