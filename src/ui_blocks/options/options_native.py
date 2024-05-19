50e5974c97572b57586b2285194454899df177b4
seruva19 <seruva19@yandex.ru>
2024-05-20 00:22:21 +0300

    add K31 "flash pipeline" option switch, change K31 text encoder to default

--- src/ui_blocks/options/options_native.py
+++ src/ui_blocks/options/options_native.py
@@ -6,9 +6,14 @@ def options_tab_native(kubin: Kubin):
     on_change = "(key, value, requiresRestart) => kubin.utils.optionsChanged(key, value, requiresRestart)"
 
     with gr.Column(elem_classes=["options-block", "options-native"]) as native_options:
+        use_kandinsky_flash = gr.Checkbox(
+            value=lambda: kubin.params("native", "use_kandinsky_flash"),
+            label="Use Kandinsky 3 Flash pipeline",
+        )
+
         text_encoder = gr.Dropdown(
             value=lambda: kubin.params("native", "text_encoder"),
-            choices=["google/flan-ul2", "pszemraj/flan-ul2-text-encoder"],
+            choices=["default", "google/flan-ul2", "pszemraj/flan-ul2-text-encoder"],
             label="Text encoder",
             allow_custom_value=True,
             elem_classes=["options-medium"],
@@ -23,6 +28,17 @@ def options_tab_native(kubin: Kubin):
         flash_attention = gr.Checkbox(
             value=lambda: kubin.params("native", "flash_attention"),
             label="Use Flash Attention",
+        )
+
+        use_kandinsky_flash.change(
+            fn=None,
+            _js=on_change,
+            inputs=[
+                gr.Text("native.use_kandinsky_flash", visible=False),
+                use_kandinsky_flash,
+                gr.Checkbox(False, visible=False),
+            ],
+            show_progress=False,
         )
 
         text_encoder.change(
