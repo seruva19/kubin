2cc334b51c01a5474b9f33985aff24ceb9f975e2
seruva19 <seruva19@yandex.ru>
2024-12-24 01:11:53 +0300

    add batch file for 'flash-attn' installation

---
+++ install-fa.bat
@@ -0,0 +1,4 @@
+call venv\Scripts\activate.bat
+pip install https://huggingface.co/lldacing/flash-attention-windows-wheel/resolve/main/flash_attn-2.7.0.post2%2Bcu124torch2.4.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
+
+PAUSE
