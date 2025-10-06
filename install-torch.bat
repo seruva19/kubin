2a624a3d64f1bf8e25068efb79e87cb4dd6ef00e
seruva19 <seruva19@yandex.ru>
2025-10-06 11:41:56 +0300

    fix typo

--- install-torch.bat
+++ install-torch.bat
@@ -1,5 +1,5 @@
 call venv\Scripts\activate.bat
 pip uninstall -y torch
-pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126 --force-reinstall --no-deps
+pip install torch==2.8.0 torchvision==0.23.0 torchaudio==2.8.0 --index-url https://download.pytorch.org/whl/cu126 --force-reinstall --no-deps
 
 PAUSE
